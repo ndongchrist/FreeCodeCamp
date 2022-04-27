703bf8626256c7919103a85d98b45e554392997c
Ndongmo Christian <christianhonore2003@gmail.com>
2022-04-27 21:05:11 +0100

    read()
    Implemeted the read() function to store all the lines contained in the
    file inside a variable and the print it.

--- Files.py
+++ Files.py
@@ -1,4 +1,7 @@
 from logging import FileHandler
-
+#Declare a FileHandler to handle the opened file
 FileHandler = open('test.txt', "r")
-print(FileHandler)
+#read() is a function used in reading the file so it stores every of
+#the file 
+inp = FileHandler.read()
+print(inp)
