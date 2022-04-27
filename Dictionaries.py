153a5e8bc147e4fea08d2879ac95459ae591a9ef
Ndongmo Christian <christianhonore2003@gmail.com>
2022-04-27 23:23:33 +0100

    implementiing a histogram uding Dictionaries

--- Dictionaries.py
+++ Dictionaries.py
@@ -6,3 +6,14 @@ print(person)
 #since five years passed since i was 18
 person['age'] += 5
 print(person)
+
+#Dictionaries are used to perform a count of a number of 
+#occurencies that occur in a list. For Example
+Names = dict()
+list = ['Akashi', 'favour','jean', 'Delia','jean','favour','Akashi','jean']
+for name in list:
+    if name not in Names:
+        Names[name] = 1
+    else:
+        Names[name] += 1
+print(Names)            
