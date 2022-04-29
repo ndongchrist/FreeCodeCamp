797c69c562c6873e183f7199495afecfb364f514
Ndongmo Christian <christianhonore2003@gmail.com>
2022-04-29 16:06:02 +0100

    NetWorking using Python
    Here i creat a socket which is a point of communication of a device. Then make
    a connection to a host data.pr4e.org to retrieve a file called romeo.txt
    so, i sent a Command to the host true the connection i creat using the socket
    and the recieved data Stored in a cariable called data.

--- Networking.py
+++ Networking.py
@@ -1,3 +1,25 @@
 import socket
 mySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mySock.connect(('data.pr4e.org', 80)) #Creating a connection using a Socket
+#for Connection to a server
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+#http is used for making the request(Asking for a document romeo.txt) 
+#where data.pr4e.org is the site and romoe.txt the name of the file
+#http is used for retrieving textfiles, documents, images etc using the 
+#GET Command.
+mySock.send(cmd)#this line of code sends the cmd accros the network established
+#by the socket
+
+#using the port 80. the server is awaiting for a command or communication that
+#is being saved in a variable called Cmd.
+
+while True:
+    #Above we are making a request now we want use a loop for collecting 
+    #the answer
+    data = mySock.recv(512)#We gonna use 256bits
+    if len(data)<1:#if the data contains no character we break the loop
+        break
+    #else we decode the data and print it
+    print(data.decode())
+    print(data)
+mySock.close()#Close the connection
